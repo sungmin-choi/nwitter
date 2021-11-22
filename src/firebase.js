0f6670375910a2ac97d358319995c29c781aa59b
sungmin-choi <namja306@naver.com>
2021-11-22 15:37:23 +0900

    Add) Firebase 및 설치

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import {initializeApp} from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyA0yZqx5C2XSDM3kxN_E0Bn-WOtL-V1JVY",
+  authDomain: "nwitter-f635a.firebaseapp.com",
+  projectId: "nwitter-f635a",
+  storageBucket: "nwitter-f635a.appspot.com",
+  messagingSenderId: "932109665107",
+  appId: "1:932109665107:web:80ad599de80ddb1da717a6"
+};
+
+export default initializeApp(firebaseConfig);
