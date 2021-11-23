1c0d47469126a9bb02106e5b1c6745bb7d45eb43
sungmin-choi <namja306@naver.com>
2021-11-23 23:55:49 +0900

    Feat) Nweets db 추가 및 불러오기 구현

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 import {initializeApp} from "firebase/app";
 import { getAuth} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain:  process.env.REACT_APP_AUTH_DOMIAN,
@@ -10,4 +11,5 @@ const firebaseConfig = {
 };
 
 initializeApp(firebaseConfig);
+export const dbService = getFirestore();
 export const authService = getAuth();
--- src/router/Home.js
+++ src/router/Home.js
@@ -1,8 +1,52 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { dbService } from "../fbase";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 const Home = () => {
+    const [Nweet,setNweet]=useState("");
+    const [Nweets,setNweets]=useState([]);
+
+    const getNweets=async()=>{
+        const dbNweets = await getDocs(collection(dbService, "Nweets"));
+        dbNweets.forEach((document) => {
+            const nweetObject={
+                ...document.data(),
+                id:document.id,
+            };
+            setNweets((prev)=>[nweetObject,...prev]);
+          });
+    }
+
+    useEffect(()=>{
+        getNweets();
+    },[]);
+    const onSubmit= async(event)=>{
+        event.preventDefault();
+        await addDoc(collection(dbService, "Nweets"), {
+            Nweet,
+            createAt: Date.now()
+        });
+        setNweet("");
+    }
+
+    const onChange=(event)=>{
+        const {target:{value}}=event;
+        setNweet(value);
+    }
+    console.log(Nweets)
+
     return(
-        <span>Home</span>
+        <>
+        <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={Nweet}  type="text" placeholder="input your mind" maxLength={120}/>
+            <input type="submit" value="Nweet"/>
+        </form>
+        <div>
+        {Nweets.map((nweet) =>{
+            return <div key={nweet.id}><h4>{nweet.Nweet}</h4></div>
+        })}
+        </div>
+        </>
+
 
     )
     
