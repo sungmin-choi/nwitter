0c0efa7a0a814fd670992ab5bec0450d71888f7c
sungmin-choi <namja306@naver.com>
2021-11-25 00:54:00 +0900

    Feat) 이미지 파일 업로드, 이미지 게시물에 시각화

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import {initializeApp} from "firebase/app";
 import { getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import {getStorage} from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain:  process.env.REACT_APP_AUTH_DOMIAN,
@@ -13,3 +14,4 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 export const dbService = getFirestore();
 export const authService = getAuth();
+export const storageService = getStorage();
--- src/router/Home.js
+++ src/router/Home.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { dbService } from "../fbase";
+import React, { useEffect, useRef, useState } from "react";
+import { dbService,storageService } from "../fbase";
 import { collection, addDoc,query,onSnapshot } from "firebase/firestore";
+import {ref, uploadString,getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from 'uuid';
 import NweetCom from "../components/Nweet";
 const Home = ({userObj}) => {
     const [Nweet,setNweet]=useState("");
     const [Nweets,setNweets]=useState([]);
     const [attachment,setAttachment]=useState(null);
+    const fileInput = useRef();
 
     useEffect(()=>{
         const q = query(collection(dbService, "Nweets"));
@@ -38,15 +41,28 @@ const Home = ({userObj}) => {
 
     const onSubmit= async(event)=>{
         event.preventDefault();
-        await addDoc(collection(dbService, "Nweets"), {
+        let fileUrl="";
+        if(attachment){
+        const uuid = uuidv4();
+        const storageRef = ref(storageService, `${userObj.uid}/${uuid}`);
+        await uploadString(storageRef, attachment, 'data_url');
+        fileUrl=await getDownloadURL(ref(storageService, `${userObj.uid}/${uuid}`));
+        }
+        const nweetObj={
             text:Nweet,
-            createAt: Date.now(),
-            creatorId:userObj.uid
-        });
+            creatorId:userObj.uid,
+            fileUrl,
+            createAt:Date.now()
+        }
+        await addDoc(collection(dbService, "Nweets"),nweetObj);
         setNweet("");
+        setAttachment(null);
     }
 
-    const cancelupload =()=> setAttachment(null);
+    const cancelupload =()=> {
+        setAttachment(null);
+        fileInput.current.value=null;
+    };
 
     const onChange=(event)=>{
         const {target:{value}}=event;
@@ -57,14 +73,13 @@ const Home = ({userObj}) => {
         <>
         <form onSubmit={onSubmit}>
             <input onChange={onChange} value={Nweet}  type="text" placeholder="input your mind" maxLength={120}/>
-            <input onChange={onChangeFile} type="file" accept="image/*" />
+            <input onChange={onChangeFile} ref={fileInput} type="file" accept="image/*" />
             <input type="submit" value="Nweet"/>
         </form>
-            {attachment?(<div>
+            {attachment && <div>
                 <img src={attachment} width="50px" height="50px"/>
                 <button onClick={cancelupload}>Cancel</button>
-            </div>)
-            :<></>}
+            </div>}
         
         <div>
         {Nweets.map((nweet) => <NweetCom key={nweet.id} NweetObj={nweet} userObj={userObj.uid===nweet.creatorId} />)}
